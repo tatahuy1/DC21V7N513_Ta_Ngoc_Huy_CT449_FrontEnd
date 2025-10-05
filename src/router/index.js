694facd9076095547101d0aa8f297a2115b4c14f
Tên của bạn <email@example.com>
2025-10-05 11:31:32 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-import AddContact from "@/views/AddContact.vue"; 
+import ContactList from "@/components/ContactList.vue";
 
 const routes = [
 {
@@ -9,15 +9,22 @@ name: "contactbook",
 component: ContactBook,
 },
 {
-    path: "/contacts/add",
-    name: "contact.add",
-    component: AddContact, 
+    path: "/contacts",
+    name: "contactlist",
+    component: ContactList,
+},
+{
+path: "/contacts/:id",
+name: "contact.edit",
+component: () => import("@/views/ContactEdit.vue"),
+props: true // Truyền các biến trong $route.params vào làm props
 },
 {
     path: "/:pathMatch(.*)*",
     name: "notfound",
     component: () => import("@/views/NotFound.vue"),
 },
+
 ];
 const router = createRouter({
 history: createWebHistory(import.meta.env.BASE_URL),
